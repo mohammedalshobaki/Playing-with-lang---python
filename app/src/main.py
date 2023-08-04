5e3dcc27234d626bb4879d4cb009df6622f2a0f8
Mohammed Alshobaki <mohammedalshobaki@Mohammeds-MacBook-Pro.local>
2023-08-04 17:33:51 +0300

    Add fileServices

--- app/src/main.py
+++ app/src/main.py
@@ -1,22 +1,6 @@
-import os
-
-class File:
-    def __init__(self, src, dest):
-        self.src = src
-        self.dest = dest
-
-  # Groups files into sub-folders based on language
-    def make_group(self):
-        files = os.listdir(self.src)
-        for file in files:
-            language, number = file.split("-")
-            language_dir = os.path.join(self.dest, language)
-            if not os.path.exists(language_dir):
-                os.mkdir(language_dir)
-            os.rename(os.path.join(self.src, file), os.path.join(language_dir, file))
-
+from services.file_service import FileService
 if __name__ == "__main__":
-    src = "./app/main/files"
-    dest = "./app/lang"
-    file = File(src, dest)
+    src = "../app/main/files"
+    dest = "../app/lang"
+    file = FileService(src, dest)
     file.make_group()
---
+++ app/src/services/file_service.py
@@ -0,0 +1,16 @@
+import os
+
+class FileService:
+    def __init__(self, src, dest):
+        self.src = src
+        self.dest = dest
+
+  # Groups files into sub-folders based on language
+    def make_group(self):
+        files = os.listdir(self.src)
+        for file in files:
+            language, number = file.split("-")
+            language_dir = os.path.join(self.dest, language)
+            if not os.path.exists(language_dir):
+                os.mkdir(language_dir)
+            os.rename(os.path.join(self.src, file), os.path.join(language_dir, file))
